b5653422bea9d664e003177f812d40933534d412
Ayush P. Bagde <ab.bagde11@gmail.com>
2022-12-05 22:58:22 +0530

    Prototype 3

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int score;
+    public TextMeshProUGUI countText;
+    public GameObject gameOverTextObject;
+    private PlayerController playerControllerScript;
+    public Transform startingPoint;
+    public float lerpSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        score = 0;
+
+        playerControllerScript.gameOver = false;
+        StartCoroutine(PlayIntro());
+        gameOverTextObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerControllerScript.gameOver)
+        {
+            if (playerControllerScript.sprint)
+            {
+                score += 2;
+            }
+            else
+            {
+                score++;
+            }
+            Debug.Log("Score: " + score);
+            countText.text = "Score : " + (score/10).ToString();
+        }
+        
+        if(playerControllerScript.gameOver)
+            gameOverTextObject.SetActive(true);
+    }
+
+    IEnumerator PlayIntro()
+    {
+        Vector3 startPos = playerControllerScript.transform.position;
+        Vector3 endPos = startingPoint.position;
+        float journeyLength = Vector3.Distance(startPos, endPos);
+        float startTime = Time.time;
+        float distanceCovered = (Time.time - startTime) * lerpSpeed;
+        float fractionOfJourney = distanceCovered / journeyLength;
+        playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 0.5f);
+
+        while (fractionOfJourney < 1)
+        {
+            distanceCovered = (Time.time - startTime) * lerpSpeed;
+            fractionOfJourney = distanceCovered / journeyLength;
+            playerControllerScript.transform.position = Vector3.Lerp(startPos, endPos,fractionOfJourney);
+            yield return null;
+        }
+        playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 1.0f);
+        playerControllerScript.gameOver = false;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtSplatter;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public float jumpForce = 12;
+    public float gravityModifier = 1.5f;
+    public float doubleJumpForce;
+    private bool isOnGround = true;
+    public bool gameOver = false;
+    public bool doubleJumpUsed = false;
+    public bool sprint = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sprint = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (sprint)
+        {
+            sprint = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
+            isOnGround = false;
+            dirtSplatter.Stop();
+            playerAudio.PlayOneShot(jumpSound, 0.8f);
+
+            doubleJumpUsed = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
+        {
+            doubleJumpUsed = true;
+            playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
+            playerAnim.Play("Running_Jump", 3, 0f);
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Ground") && !gameOver)
+        {
+           isOnGround = true;
+           dirtSplatter.Play();
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b",true);
+            playerAnim.SetInteger("DeathType_int",1);
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(crashSound, 1f);
+            dirtSplatter.Stop();
+            
+        }
+    }
+}
